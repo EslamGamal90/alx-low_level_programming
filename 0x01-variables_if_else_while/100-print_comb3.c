be666cdfc730bab87e54e6434435c5f54753a19f
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-17 11:55:29 -0800

    task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 for (a = '0'; a < '9'; a++)
 {
-		for (b = a + 1; b <= '9'; b++)
+	for (b = a + 1; b <= '9'; b++)
 		{
-			if (a != b)
+		if (a != b)
 				{
 				putchar(a);
 				putchar(b);
-				if (a == '8' && b == '9')
+			if (a == '8' && b == '9')
 				continue;
 				putchar(44);
 				putchar(' ');
