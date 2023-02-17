533e5429f99ef22c65906294bd110e5317775ae2
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-17 10:16:10 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Print if number is positive, Zero or negative
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int b;
+
+	for (b = '0'; b <= '9'; b++)
+	putchar(b);
+	putchar('\n');
+	return (0);
+}
