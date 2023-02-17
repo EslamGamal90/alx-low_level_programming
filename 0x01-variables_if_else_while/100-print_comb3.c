ac8b90cc948c6b112dd81bef3e006d509fcc7ac3
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-17 11:48:25 -0800

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Print if number is positive, Zero or negative
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a, b;
+
+for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			if (a != b)
+				{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				continue;
+				putchar(44);
+				putchar(' ');
+				}
+			else
+			{}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
