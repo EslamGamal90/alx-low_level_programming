24815dc52daf47d2ba6652e26f2cf7746c55e8e7
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-17 09:55:51 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Print if number is positive, Zero or negative
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q')
+		{
+		}
+		else if (c == 'e')
+		{
+		}
+		else
+			putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
