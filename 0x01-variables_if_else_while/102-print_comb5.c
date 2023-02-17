e8651a981e9e79a1baebe9826a5fd0de19e31d8b
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-17 13:31:38 -0800

    task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i <= 98; i++)
 	{
-		for (j = i+1; j <= 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
-			putchar((j % 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
-			putchar((i / 10) + '0');
+			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
 			continue;
