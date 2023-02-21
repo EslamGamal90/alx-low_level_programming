f1d054d94c676917654b4b4f5667aa186cbf082c
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-21 11:09:32 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - a simple program that print _putchar
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	retuen 0;
+}
