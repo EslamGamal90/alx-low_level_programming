7a6bfcd43a3873eb0bcf98e3ef62fa1f9b9ceaa2
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-21 13:01:23 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check for char alphabet
+ *@n: num for check
+ *Return: the abs of int
+ */
+int _abs(int n)
+{
+if (n >= 0)
+	{
+	return (n);
+	}
+else
+	return (-n);
+}
