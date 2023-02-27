22be04ddaeac019a960c32972e31a9adaf20ea0f
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-27 09:59:53 -0800

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * @a: This is the entry
+ * @b: This is the entry
+ */
+
 void swap_int(int *a, int *b)
 {
-	int x ;
-	x= *a;
+	int x;
+
+	x = *a;
 	*a = *b;
 	*b = x;
 }
