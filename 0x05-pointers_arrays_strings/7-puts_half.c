180878caf696bb03cb7830737645de70b64256f8
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-27 12:21:35 -0800

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - This fuction print a string reverser
+ *
+ *@str: this is the pointer that point to a string
+ */
+
+void puts_half(char *str)
+{
+	for (int x = 0; s[i] != '\0'; x++)
+		{
+	if (i % 2 != 0)
+		{
+		_putchar(s[i]);
+		}
+		}
+		_putchar('\n');
+}
