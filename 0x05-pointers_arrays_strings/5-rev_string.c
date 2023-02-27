d372bfe31a0d88e028a3880bc7f2a3ee1635f786
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-27 11:19:34 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,29 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * rev_string - Function that returns the length of a string
+ * print_rev - Function that returns the length of a string
  *
  * @s: this is the input string
- *
  *
  */
 
 void rev_string(char *s)
 {
-	int i;
+	int i, x;
+	char *c;
+	char *z;
 
 	for (i = 0; s[i] != '\0'; i++)
-		;
-	for (i = i - 1; i >= 0 ; i--)
+			{
+			z[i] = s[i];
+			_putchar(s[i]);
+			}
+			_putchar('\n');
+	for (i = i - 1, x = 0 ; i >= 0 ; i--, x++)
 		{
+		c[x] = s[i];
 		_putchar(s[i]);
+		s[i] = z[x];
 		}
 		_putchar('\n');
 }
