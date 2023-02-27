e2f438e5b24999028b3bbe1d2f5d711c88b24242
EslamGamal90 <engeslamgamal@outlook.com>
2023-02-27 12:02:45 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - This fuction print a string reverser
+ *
+ * @str: this is the pointer that point to a string
+ */
+
+void puts2(char *str)
+{
+	for (int x = 0; s[i] != '\0'; x++)
+		{
+		_putchar(s[i]);
+		x++;
+		}
+		_putchar('\n');
+}
